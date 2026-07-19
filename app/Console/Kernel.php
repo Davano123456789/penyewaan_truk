2e376e299fdedd9728065abf0f3bf6b7377448d5
DAVANO ALIF RAMADHAN <yustinamalia22@gmail.com>
2026-07-19 18:32:17 +0700

    update kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('booking:clean')->everyTenMinutes();
+        $schedule->command('booking:clean')->everyTwoMinutes();
     }
 
     /**
